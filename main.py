1f6c48ba466d34ef4f2a16ff26d920750b6c9b60
valya_plotnikova <valya_ezhova@mail.ru>
2025-01-11 17:45:20 +0300

    create config for async

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import asyncio
+
 from models.database import create_db, drop_db
-from parser import parsing_trading_on_file, get_data, save_data_to_db
+# from parser import parsing_trading_on_file, get_data, save_data_to_db
 
 
-def main():
-    drop_db()
-    create_db()
-    trade_date = parsing_trading_on_file()
-    spimex_trading_results = get_data(trade_date)
-    save_data_to_db(spimex_trading_results)
+async def main():
+    await drop_db()
+    await create_db()
+    # trade_date = parsing_trading_on_file()
+    # spimex_trading_results = get_data(trade_date)
+    # save_data_to_db(spimex_trading_results)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
--- models/database.py
+++ models/database.py
@@ -1,21 +1,25 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 Base = declarative_base()
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
+engine = create_async_engine(DATABASE_URL, pool_pre_ping=True)
+Session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
-def create_db():
-    Base.metadata.create_all(bind=engine)
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     print('Таблица успешно создана')
 
 
-def drop_db():
-    Base.metadata.drop_all(bind=engine)
+async def drop_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    print('Таблица успешно удалена')
